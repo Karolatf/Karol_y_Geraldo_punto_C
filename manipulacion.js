dad74f4dc05f8f1c4a2349507b04272af81210e8
karol <karoln.oficiall@gmail.com>
2025-11-12 19:45:35 -0500

    primer ejercicio en parejas

---
+++ manipulacion.js
@@ -0,0 +1,23 @@
+// a) Manipulación de Arreglos
+// a. Cree un arreglo llamado verduras con al menos 4 elementos.
+// b. Realice las siguientes operaciones y muestre el resultado en consola después
+// de cada una:
+// push() // agregar un nuevo elemento al final
+// pop() // eliminar el último elemento
+// unshift() // agregar un nuevo elemento al inicio
+// shift() // eliminar el primer elemento
+// c. Finalmente, muestre cuántos elementos contiene el arreglo usando length.
+
+
+let verduras = ['lechuga', 'tomate', 'zanahoria', 'pepino']; //a//
+verduras.push ('espinaca'); //b.1//
+console.log(verduras);
+verduras.pop (); //b.2//
+console.log(verduras);
+verduras.unshift ('remolacha'); //b.3//
+console.log(verduras);
+verduras.shift (); //b.4//
+console.log(verduras);
+
+verduras.length; //c//
+console.log(verduras.length);
